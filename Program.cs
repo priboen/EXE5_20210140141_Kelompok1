dd5a89ebc700935efea467b52959044cab4ba179
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-12-21 15:43:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20210140141_Kelompok1
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
